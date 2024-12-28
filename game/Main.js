251c7f6cd2aeeb2361d1b85cc70afdb161bbdc2e
Stpv22 <145866689+STPv22@users.noreply.github.com>
2024-12-28 19:01:47 +0000

    this is about as far as i got before i lost motovation

---
+++ game/Main.js
@@ -0,0 +1,7 @@
+
+function init() {
+    const canvas = document.createElement("canvas");
+    canvas.id = 'game_container';
+    const ctx = canvas.getContext('2d');
+    const canvasOffsetX = canvas.offsetLeft
+}
