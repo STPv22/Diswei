b7ebc10cf245b8cf99a4a387cfec4d7003f7385f
Stpv22 <145866689+STPv22@users.noreply.github.com>
2024-12-30 23:06:40 +0000

    |

--- game/Main.js
+++
@@ -1,7 +0,0 @@
-
-function init() {
-    const canvas = document.createElement("canvas");
-    canvas.id = 'game_container';
-    const ctx = canvas.getContext('2d');
-    const canvasOffsetX = canvas.offsetLeft
-}
